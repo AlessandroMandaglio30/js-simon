19ae852c9b8a66bc49e8537daff7bb68da0641cb
Alessandro Mandaglio <ale.manda.30@gmail.com>
2021-11-24 16:01:42 +0100

    Eserczio

---
+++ js/script.js
@@ -0,0 +1,56 @@
+let difficolta = parseInt(prompt('Con quanti numeri vuoi giocare?'));
+
+for (let i = 0; i < 1; i++) {
+    if (isNaN(difficolta)) {
+        difficolta = parseInt(prompt('Alessandro ti dice di inserire i numeri'));
+        i--;
+    }
+}
+
+let numeriCasuali = [];
+
+for (let i = 0; i < difficolta; i++) {
+    let numRandom = Math.floor(Math.random() * 100) + 1;
+    if (numeriCasuali.includes(numRandom)) {
+        i--;
+    } else {
+        numeriCasuali.push(numRandom);
+    }
+}
+
+let seconds = 30;
+
+let display = document.getElementById('timer');
+
+let timerDisplay = setInterval(function () {
+
+    display.innerHTML = seconds;
+
+    if (seconds == 0) {
+        clearInterval(timerDisplay)
+    } else {
+        seconds--;
+    }
+
+}, 1000);
+
+alert('Numeri da ricordare: ' + numeriCasuali);
+
+setTimeout(timer, 30000);
+
+function timer() {
+    let arrayNum = [];
+    let score = 0;
+    for (let i = 0; i < difficolta; i++) {
+        let numeriGioco = parseInt(prompt('Alessandro dice di riscrivere i numeri che hai visto'));
+        if (numeriCasuali.includes(numeriGioco)) {
+            arrayNum.push(numeriGioco);
+            score++;
+            alert('Corretto!');
+        } else {
+            alert('Sbagliato!');
+        }
+    }
+    alert('Hai indovinato: ' + arrayNum + ' per un punteggio di ' + score);
+    console.log('Hai indovinato: ' + arrayNum + ' per un punteggio di ' + score);
+}
